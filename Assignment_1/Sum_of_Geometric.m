c0b5a32ba2a546b447a8cecdc5a342f82f49056e
vishwa-patwari <vishwakpatwari@gmail.com>
2024-10-27 16:40:27 +0530

    Update Sum_of_Geometric.m

--- Assignment_1/Sum_of_Geometric.m
+++ Assignment_1/Sum_of_Geometric.m
@@ -1,10 +1,8 @@
 a = input('Enter the value of a (first term): ');
 r = input('Enter the value of r (common ratio): ');
 n = input('Enter the value of n (number of terms): ');
-
 S = 0;
 i = 0;
-
 while i <= n
     S = S + a * r^i;
     i = i + 1;
